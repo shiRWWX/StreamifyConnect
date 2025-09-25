5e4414b76f43d9480770acf23d099b6584c02200
Shiva Bharati <shivabharati153@gmail.com>
2025-09-25 18:09:07 +0530

    Initial commit: frontend + backend

--- frontend/src/pages/OnboardingPage.jsx
+++ frontend/src/pages/OnboardingPage.jsx
@@ -3,7 +3,8 @@ import useAuthUser from "../hooks/useAuthUser";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import { completeOnboarding } from "../lib/api";
-import { LoaderIcon, MapPinIcon, ShipWheelIcon, ShuffleIcon } from "lucide-react";
+import { CameraIcon, LoaderIcon, MapPinIcon, ShipWheelIcon, ShuffleIcon } from "lucide-react";
+
 import { LANGUAGES } from "../constants";
 
 const OnboardingPage = () => {
